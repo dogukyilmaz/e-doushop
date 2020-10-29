3211e6a11a4357099bc547186c6a986a965f3680
Kaan Yilmaz <douscriptist@gmail.com>
2020-10-29 23:27:42 +0300

    product type and environment

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express, { Application } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
 
 import { products } from "./data/products";
+import { Product } from "types";
 
 const app: Application = express();
+
+app.use(cors());
 
 const PORT = 4500;
 
@@ -10,6 +17,13 @@ app.get("/", (req, res) => res.send("Express + TypeScript Server"));
 
 app.get("/api/v1/products", (req, res) => {
   res.json(products);
+});
+
+app.get("/api/v1/products/:id", (req, res) => {
+  const response = products.filter((prod: Product) => prod._id === req.params.id);
+  setTimeout(() => {
+    res.json(response);
+  }, 2000);
 });
 
 app.listen(PORT, () => {
