18321637b2c658f258cc54526dd89f7ef3f981fa
Kaan Yilmaz <douscriptist@gmail.com>
2020-11-13 17:42:15 +0300

    get order/s

--- src/controller/order.ts
+++ src/controller/order.ts
@@ -1,7 +1,6 @@
 import asyncHandler from "express-async-handler";
 import { NextFunction, Request, Response } from "express";
 
-import Product from "models/Product";
 import Order from "models/Order";
 
 // @description   Create order
@@ -28,4 +27,37 @@ export const addOrder = asyncHandler(async (req: Request, res: Response, next: N
     const createdOrder = await order.save();
     res.status(201).json({ success: true, data: createdOrder });
   }
+});
+
+// @description   Get order by id
+// @route         GET /api/v1/orders/:id
+// @access        Private
+export const getSingleOrder = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  const order = await Order.findById(req.params.id).populate("user", "name email");
+
+  if (!order) {
+    res.status(404);
+    throw new Error("Order not found!");
+  }
+
+  if (order.user !== req.user?._id) {
+    res.status(401);
+    throw new Error("Not authorized!");
+  }
+
+  res.json({ success: true, data: order });
+});
+
+// @description   Get all orders
+// @route         GET /api/v1/orders
+// @access        Private
+export const getAllOrders = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  const orders = await Order.find({ user: req.user?._id }).populate("user", "name email");
+
+  if (!orders.length) {
+    res.status(404);
+    throw new Error("There is no order, yet.");
+  }
+
+  res.json({ success: true, data: orders });
 });
--- src/routes/order.ts
+++ src/routes/order.ts
@@ -1,9 +1,11 @@
-import express, { Router, Request, Response, NextFunction } from "express";
+import express, { Router } from "express";
 import * as service from "controller/order";
 import { authenticate } from "middlewares/auth";
 
 const router: Router = express.Router();
 
 router.post("/", authenticate, service.addOrder);
+router.get("/:id", authenticate, service.getSingleOrder);
+router.get("/", authenticate, service.getAllOrders);
 
 export default router;
